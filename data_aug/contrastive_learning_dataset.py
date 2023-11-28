3be074e796473701311986f6c3efeeafebb3f995
Neo Yin <yinzhang@berkeley.edu>
2023-11-27 17:33:55 -0800

    updated the training script

--- data_aug/contrastive_learning_dataset.py
+++ data_aug/contrastive_learning_dataset.py
@@ -4,6 +4,9 @@ from torchvision import transforms, datasets
 from data_aug.view_generator import ContrastiveLearningViewGenerator
 from exceptions.exceptions import InvalidDatasetSelection
 
+import os
+from wsissl.dataset import PFDataset, folder_train_test_split
+
 
 class ContrastiveLearningDataset:
     def __init__(self, root_folder):
@@ -13,30 +16,56 @@ class ContrastiveLearningDataset:
     def get_simclr_pipeline_transform(size, s=1):
         """Return a set of data augmentation transformations as described in the SimCLR paper."""
         color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
-        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=size),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.RandomApply([color_jitter], p=0.8),
-                                              transforms.RandomGrayscale(p=0.2),
-                                              GaussianBlur(kernel_size=int(0.1 * size)),
-                                              transforms.ToTensor()])
+        data_transforms = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(size=size),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomApply([color_jitter], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                GaussianBlur(kernel_size=int(0.1 * size)),
+                transforms.ToTensor(),
+            ]
+        )
         return data_transforms
 
-    def get_dataset(self, name, n_views):
-        valid_datasets = {'cifar10': lambda: datasets.CIFAR10(self.root_folder, train=True,
-                                                              transform=ContrastiveLearningViewGenerator(
-                                                                  self.get_simclr_pipeline_transform(32),
-                                                                  n_views),
-                                                              download=True),
-
-                          'stl10': lambda: datasets.STL10(self.root_folder, split='unlabeled',
-                                                          transform=ContrastiveLearningViewGenerator(
-                                                              self.get_simclr_pipeline_transform(96),
-                                                              n_views),
-                                                          download=True)}
+    def get_dataset(self, name, n_views, num_images_per_epoch):
+        valid_datasets = {
+            "cifar10": lambda: datasets.CIFAR10(
+                self.root_folder,
+                train=True,
+                transform=ContrastiveLearningViewGenerator(
+                    self.get_simclr_pipeline_transform(32), n_views
+                ),
+                download=True,
+            ),
+            "stl10": lambda: datasets.STL10(
+                self.root_folder,
+                split="unlabeled",
+                transform=ContrastiveLearningViewGenerator(
+                    self.get_simclr_pipeline_transform(96), n_views
+                ),
+                download=True,
+            ),
+        }
 
         try:
             dataset_fn = valid_datasets[name]
         except KeyError:
-            raise InvalidDatasetSelection()
+            if os.path.isdir(name):
+                train_folders, _ = folder_train_test_split(
+                    os.path.join(name, "train"), train_prop=1
+                )
+                train_dataset = PFDataset(
+                    folders=train_folders,
+                    num_images_per_epoch=num_images_per_epoch,  # args.num_images_per_epoch,
+                    transform=ContrastiveLearningViewGenerator(
+                        self.get_simclr_pipeline_transform(96), n_views
+                    ),
+                )
+                # print(train_dataset)
+
+                return train_dataset
+            else:
+                raise InvalidDatasetSelection()
         else:
             return dataset_fn()
